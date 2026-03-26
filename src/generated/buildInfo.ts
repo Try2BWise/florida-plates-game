1974d42a7430049b601a2c4b0dda27733100f790
Brandon <brandon.m.wise@gmail.com>
2026-03-26 00:52:06 -0400

    Update icons and build info

--- src/generated/buildInfo.ts
+++ src/generated/buildInfo.ts
@@ -1,6 +1,6 @@
 export const buildInfo = {
   version: "1.4.0",
-  builtAtIso: "2026-03-26T04:50:50.092Z",
+  builtAtIso: "2026-03-26T04:51:36.762Z",
   branch: "master",
-  commit: "52a7954"
+  commit: "45885f6"
 } as const;
